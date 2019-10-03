66b04e863f55c46a6c718b02f12c3a2233101c25
LYSmaybe <623845536@qq.com>
2019-10-03 19:28:39 +0800

    05

---
+++ 05/05/Case conversion.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int ch = 0;
+	while ((ch = getchar()) != EOF)
+	{
+		if (ch >= 'a' && ch <= 'z')
+		{
+			putchar(ch - 32);
+		}
+		else if (ch >= 'A' && ch <= 'Z')
+		{
+			putchar(ch + 32);
+		}
+		else if (ch >= '0' && ch <= '9')
+		{
+			;
+		}
+	}
+	return 0;
+}
