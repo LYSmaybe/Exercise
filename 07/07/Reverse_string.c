2d5bd8eeba66178b227b906c6f984dcf9e05e240
LYSmaybe <623845536@qq.com>
2019-10-05 15:08:42 +0800

    07

--- 07/07/Reverse_string.c
+++ 07/07/Reverse_string.c
@@ -1,19 +1,23 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-void reverse_string(char* str,int sz)
+void reverse_string(char* string,int sz)
 {
 	if (sz)
 	{
-		printf("%s", str+sz);
-		reverse_string(str,sz-1);
+		printf("%s", string);
+		return reverse_string(string,sz-1);
+	}
+	else
+	{
+		return 0;
 	}
 }
 
 int main()
 {
 	char arr[] = "abcdefg";
-	int sz = sizeof(arr) / sizeof(arr[0])-1;
-	reverse_string(arr, sz);
+	int sz = sizeof(arr) / sizeof(arr[0]);
+ 	reverse_string(& arr,sz);
 	return 0;
 }
